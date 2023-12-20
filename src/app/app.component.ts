e13e40c6e5f3a24a10451a469c8996929417ba3f
Reza Madahi <reza.maddahi@outlook.com>
2023-12-20 05:59:49 +0330

    working with standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {ServerComponent} from "./server/server.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, ServerComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angular-test-project';
 }
